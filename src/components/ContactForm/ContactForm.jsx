af71e9f4410e328fc9dc66b9133a763519d0c064
ZoyaBukhantseva <zoyapashchenko87@gmail.com>
2023-02-07 21:40:10 +0300

    add

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,9 +1,60 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import styles from './ContactForm.module.css';
 import PropTypes from 'prop-types';
 
-class ContactForm extends Component {
+const initialState = {
+  name: "",
+  number: "",
+};
+const ContactForm= ({onSubmit})=> {
+  const [state, setState] = useState({...initialState})
+
+  const handleChange = ({target}) => {
+      const {name, value} = target;
+      setState(prevState => {
+          return {...prevState, [name]: value}
+      })
+  }
+
+  const handleSubmit = e => {
+      e.preventDefault();
+      onSubmit({name, number});
+      setState({...initialState});
+  }
+
+   
+  return (
+    <>
+      <form  className={styles.formwrap}  onSubmit={this.handleSubmit}>
+        <label className={styles.contactFormLable}>
+          <input
+          className={styles.input}
+            onChange={this.inputHandler}
+            type="text"
+            name="name"
+            placeholder="Enter name..."
+            value={name}
+          ></input>
+        </label>
+        <label className={styles.contactFormLable}>
+          <input
+          className={styles.input}
+            onChange={this.inputHandler}
+            type="tel"
+            name="number"
+            placeholder="Enter number..."
+            value={number}
+          ></input>
+        </label>
+
+        <button className={styles.btn} type="submit">Add contact</button>
+      </form>
+    </>
+  );
+  }
+
+/*class ContactForm extends Component {
   initialState = {
     name: '',
     number: '',
@@ -65,5 +116,5 @@ class ContactForm extends Component {
 ContactForm.propTypes = {
   addToPhonebook: PropTypes.func.isRequired,
 };
-
+*/
 export default ContactForm;
